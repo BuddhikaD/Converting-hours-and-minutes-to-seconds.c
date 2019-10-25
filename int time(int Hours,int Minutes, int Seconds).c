9c6796f0ae3b31bd7364ba0678380b7dff742f1c
Buddhiofficial <56988948+Buddhiofficial@users.noreply.github.com>
2019-10-25 09:47:36 +0530

    Update int time(int Hours,int Minutes, int Seconds).c

--- int time(int Hours,int Minutes, int Seconds).c
+++ int time(int Hours,int Minutes, int Seconds).c
@@ -4,23 +4,23 @@ return Hours%12 * 3600 + Minutes*60 + Seconds;
 }
 int main ()
 {
-int h1,m1,s1,h2,m2,s2;
+int p1,m1,s1,p2,m2,s2;
 printf("Enter the time [Hours:Minutes:Seconds] : ");
 scanf("%d %d %d",&h1,&m1,&s1);
-if(h1>24 || m1>59 || s1>59)
+if(p1>24 || m1>59 || s1>59)
 {
 printf("\nInvalid time format\n");
 exit(0);
 }
 printf("Enter the time [Hours:Minutes:Seconds] : ");
-scanf("%d %d %d",&h2,&m2,&s2);
-if(h1>24 || m1>59 || s1>59)
+scanf("%d %d %d",&p2,&m2,&s2);
+if(p1>24 || m1>59 || s1>59)
 {
 printf("\nInvalid time format\n");
 exit(0);
 }
-if(time(h1,m1,s1)>time(h2,m2,s2))
-printf("\n\t%d Seconds",time(h1,m1,s1) - time(h2,m2,s2));
+if(time(p1,m1,s1)>time(p2,m2,s2))
+printf("\n\t%d Seconds",time(p1,m1,s1) - time(p2,m2,s2));
 else
-printf("\n\t%d Seconds",time(h2,m2,s2) - time(h1,m1,s1));
+printf("\n\t%d Seconds",time(p2,m2,s2) - time(p1,m1,s1));
 }
